6afd798d66c354c989c654545ab4e8d569845ae6
beifeng <1497871755@qq.com>
2022-06-25 10:02:57 +0800

    3

--- src/main/java/com/atguigu/test2.java
+++ src/main/java/com/atguigu/test2.java
@@ -4,5 +4,7 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("222222222222");
         System.out.println("333");
+        System.out.println("34");
+        System.out.println("5555");
     }
 }
